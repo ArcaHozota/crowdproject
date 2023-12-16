a4a5154c15592bcb5359ec47fca3b425635b90de
ArkamaHozota <arkamahozota@gmail.com>
2023-12-16 22:46:53 +0900

    1.03

--- src/main/java/jp/co/sony/ppog/mapper/EmployeeMapper.java
+++ src/main/java/jp/co/sony/ppog/mapper/EmployeeMapper.java
@@ -1,7 +1,11 @@
 package jp.co.sony.ppog.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import jp.co.sony.ppog.entity.Employee;
 
 /**
  * 社員マッパー
@@ -20,9 +24,46 @@ public interface EmployeeMapper {
 	Integer checkDuplicated(@Param("loginAccount") String loginAccount);
 
 	/**
+	 * IDによって論理削除を行う
+	 *
+	 * @param id 社員ID
+	 */
+	void deleteById(Long id);
+
+	/**
+	 * IDによって情報を挿入する
+	 *
+	 * @param employee 社員エンティティ
+	 */
+	void insertById(Employee employee);
+
+	/**
 	 * ID採番値を取得する
 	 *
 	 * @return Long
 	 */
 	Long saiban();
+
+	/**
+	 * IDによって情報を検索する
+	 *
+	 * @param id 社員ID
+	 * @return Employee
+	 */
+	Employee selectById(Long id);
+
+	/**
+	 * キーワードによって社員情報を検索する
+	 *
+	 * @param keyword 検索キーワード
+	 * @return List<Employee>
+	 */
+	List<Employee> selectByKeyword(String keyword);
+
+	/**
+	 * IDによって情報を更新する
+	 *
+	 * @param employee 社員エンティティ
+	 */
+	void updateById(Employee employee);
 }
