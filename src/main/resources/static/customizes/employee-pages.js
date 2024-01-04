cf695e1bf447baef2031139cc02fca03fc9fdcb6
ArkamaHozota <arkamahozota@hotmail.com>
2024-01-04 16:24:20 +0900

    3.34

--- src/main/resources/static/customizes/employee-pages.js
+++ src/main/resources/static/customizes/employee-pages.js
@@ -96,7 +96,7 @@ $("#saveInfoBtn").on('click', function() {
 	let inputPassword = $("#passwordInput").val().trim();
 	let inputEmail = $("#emailInput").val().trim();
 	let inputRole = $("#roleInput").val();
-	if ($("#inputForm").children().hasClass('is-invalid')) {
+	if ($("#inputForm").find('*').hasClass('is-invalid')) {
 		return false;
 	} else if (inputLoginAccount === "" || inputUsername === "" || inputPassword === "" || inputEmail === "") {
 		let listArray = ["#loginAccountInput", "#usernameInput", "#passwordInput", "#emailInput"];
@@ -166,7 +166,7 @@ $("#editInfoBtn").on('click', function() {
 	let editPassword = $("#passwordEdit").val().trim();
 	let editEmail = $("#emailEdit").val().trim();
 	let editRole = $("#roleEdit option:selected").val();
-	if ($("#editForm").children().hasClass('is-invalid')) {
+	if ($("#editForm").find('*').hasClass('is-invalid')) {
 		return false;
 	} else if (editUsername === "" || editPassword === "" || editEmail === "") {
 		let listArray = ["#usernameEdit", "#passwordEdit", "#emailEdit"];
